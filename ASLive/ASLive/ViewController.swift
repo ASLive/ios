98816e545fbac79401cd53e0a3c5a0a8ee9b0843
anvitamishra <anvitamishra309@gmail.com>
2018-11-09 00:45:55 -0500

    Set up iOS app

---
+++ ASLive/ASLive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASLive
+//
+//  Created by Anvita Mishra on 11/8/18.
+//  Copyright © 2018 Anvita Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
